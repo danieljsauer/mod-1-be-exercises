3e3e01c236b441218ed20e2093a023852f945ea8
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-03 12:51:44 -0600

    complete the dragon rspec

---
+++ ruby_exercises/mythical-creatures/lib/dragon.rb
@@ -0,0 +1,23 @@
+class Dragon
+attr_reader :name, :color, :rider
+
+    def initialize(name,color,rider)
+        @name = name
+        @rider = rider
+        @color = color
+        @hungry = true
+        @counter = 0
+    end
+
+    def hungry?
+        @hungry
+    end
+
+    def eat
+    @counter = @counter +=1
+        if @counter >= 3
+            @hungry = false
+        end 
+    end 
+
+end 
